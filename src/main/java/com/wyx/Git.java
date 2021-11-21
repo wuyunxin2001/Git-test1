efe7b1cb99e8434614b54fad160e6dba1829ac40
wuyunxin <94663690+wuyunxin2001@users.noreply.github.com>
2021-11-21 09:36:17 +0800

    idea first commit

---
+++ src/main/java/com/wyx/Git.java
@@ -0,0 +1,11 @@
+package com.wyx;
+
+/**
+ * @author wyx
+ * @date 2021/11/21-9:32
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
