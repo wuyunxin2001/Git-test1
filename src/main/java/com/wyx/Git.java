a892dc9d3339056b80ae52af6c0f2a5bb1a96bb3
wuyunxin <94663690+wuyunxin2001@users.noreply.github.com>
2021-11-21 09:39:09 +0800

    idee third commit

--- src/main/java/com/wyx/Git.java
+++ src/main/java/com/wyx/Git.java
@@ -8,5 +8,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello GitHub");
+        System.out.println("hello third");
+
     }
 }
